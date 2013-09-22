e74faab2815c081c642c67a15c556c92ad39434e
sachin-rajput <sachin.rajput@hotmail.com>
2013-09-22 16:37:27 -0700

    Added code for threading and PopulateWorker

--- src/dTunesStore/driver/Driver.java
+++ src/dTunesStore/driver/Driver.java
@@ -11,11 +11,10 @@ import dTunesStore.util.Helper;
 import dTunesStore.dataStore.PopulateWorker;
 import dTunesStore.dataStore.SearchWorker;
 
-
 /**
  * @author1 Sachin Rajput
  * @author2 Amol Mankar
- *
+ * 
  */
 public class Driver {
 
@@ -24,48 +23,52 @@ public class Driver {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		/***
 		 * Set DEBUG_VALUE
 		 */
-		
+
 		Debug debug = new Debug();
 		debug.set_debug_value(Integer.parseInt(args[4]));
-		
-		System.out.println("DEBUG_VALUE set to : "+ debug.get_debug_value());
-		
+
+		System.out.println("DEBUG_VALUE set to : " + debug.get_debug_value());
+
 		/***
-		 * Step 1: Open the dataFile for reading check change 
+		 * Step 1: Open the dataFile for reading check change
 		 */
-		
-		String file_name = args[0]+".txt";
-		
-		new PopulateWorker(); // create a new thread
-		new PopulateWorker(); 
-		
+
+		String file_name = args[0] + ".txt";
+
 		/***
-		 * Create threads 
+		 * Create threads
 		 */
-		
+
 		try {
-			// Open the file that is the first
-			// command line parameter
-			FileInputStream fstream = new FileInputStream(file_name);
-			// Get the object of DataInputStream
-			DataInputStream in = new DataInputStream(fstream);
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			String strLine;
-			// Read File Line By Line
-			while ((strLine = br.readLine()) != null) {
-				// Print the content on the console
-				System.out.println(strLine);
-			}
 			
+			
+			/***
+			 * Get number of lines in input file
+			 */
 			Helper helper = new Helper();
-			System.out.println("The number of lines : " + helper.countLines(file_name));
-				
-			// Close the input stream
-			in.close();
+			int numberOflines = helper.countLines(file_name);
+			System.out.println("The number of lines : " + numberOflines);
+
+			/***
+			 * Divide the chunks
+			 */
+			int worker_threads = Integer.parseInt(args[1]);
+			int search_threads = Integer.parseInt(args[3]);
+			int chunk_size = numberOflines / worker_threads;
+			
+			int from = 0;
+			int to = chunk_size-1;
+			
+			for (int i = 0; i < worker_threads; i++) {
+				new PopulateWorker(file_name,from,to);
+				from = to + 1;
+				to = from + chunk_size - 1;
+			}
+
 		} catch (Exception e) {// Catch exception if any
 			System.err.println("Error: " + e.getMessage());
 		}
