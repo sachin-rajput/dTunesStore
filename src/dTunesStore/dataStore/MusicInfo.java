1389a4012485cb42269dc939115deeb2de2dc4d0
amol-mankar <amankar1@binghamton.edu>
2013-09-22 19:45:37 -0400

    Update MusicInfo.java
    Added data members for musicInfo and setters and getters

--- src/dTunesStore/dataStore/MusicInfo.java
+++ src/dTunesStore/dataStore/MusicInfo.java
@@ -2,4 +2,45 @@ package dTunesStore.dataStore;
 
 public class MusicInfo {
 
+	private String songName;
+	
+	private String albumName;
+	
+	private String leadName;
+	
+	private double duration;
+
+	public String getSongName() {
+		return songName;
+	}
+
+	public void setSongName(String songName) {
+		this.songName = songName;
+	}
+
+	public String getAlbumName() {
+		return albumName;
+	}
+
+	public void setAlbumName(String albumName) {
+		this.albumName = albumName;
+	}
+
+	public String getLeadName() {
+		return leadName;
+	}
+
+	public void setLeadName(String leadName) {
+		this.leadName = leadName;
+	}
+
+	public double getDuration() {
+		return duration;
+	}
+
+	public void setDuration(double duration) {
+		this.duration = duration;
+	}
+	
+	
 }
