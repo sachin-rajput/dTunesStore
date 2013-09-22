bdd87f80e634d35ab8b522abb0536344bd34c40a
sachin-rajput <sachin.rajput@hotmail.com>
2013-09-22 11:02:11 -0700

    Updated Debug.java Added code for handling Debug Messages : The DEBUG_VALUE should be accordingly set in the class Debug. Use the DEBUG_VALUE in the following way: DEBUG_VALUE=4 [Print to stdout everytime a constructor is called] DEBUG_VALUE=3 [Print to stdout everytime a thread's run() method is called] DEBUG_VALUE=2 [Print to stdout everytime an entry is added to the Results data structure] DEBUG_VALUE=1 [Print to stdout the search results] DEBUG_VALUE=0 [No output should be printed from the application]

--- util/dTunesStore/util/Debug.java
+++ util/dTunesStore/util/Debug.java
@@ -4,5 +4,16 @@ public class Debug {
 	private static int DEBUG_VALUE;
 	
 	//accessor and mutator for DEBUG_VALUE
+	public void set_debug_value(int value) {
+		this.DEBUG_VALUE = value;
+	}
 	
+	public int get_debug_value() {
+		return this.DEBUG_VALUE;
+	}
+	
+	public void print_debug(int value,String Message) {
+		if(this.get_debug_value() == value)
+			System.out.println(Message);
+	}
 }
