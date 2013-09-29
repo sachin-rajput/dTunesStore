8d9abb6f1d951e6d731e9742264101c4422175a8
sachin-rajput <sachin.rajput@hotmail.com>
2013-09-29 16:45:27 -0700

    Added comments

--- src/dTunesStore/dataStore/MusicInfo.java
+++ src/dTunesStore/dataStore/MusicInfo.java
@@ -10,34 +10,66 @@ public class MusicInfo {
 	
 	private double duration;
 
+	/**
+	 * Accessor for songName 
+	 * @return songName
+	 */
 	public String getSongName() {
 		return songName;
 	}
-
+	
+	/**
+	 * Mutator for songName
+	 * @param songName
+	 */
 	public void setSongName(String songName) {
 		this.songName = songName;
 	}
 
+	/**
+	 * Accessor for albumName
+	 * @return albumName
+	 */
 	public String getAlbumName() {
 		return albumName;
 	}
 
+	/**
+	 * Mutator for albumName
+	 * @param albumName
+	 */
 	public void setAlbumName(String albumName) {
 		this.albumName = albumName;
 	}
 
+	/**
+	 * Accessor for leadName
+	 * @return leadName
+	 */
 	public String getLeadName() {
 		return leadName;
 	}
 
+	/**
+	 * Mutator for leadName
+	 * @param leadName
+	 */
 	public void setLeadName(String leadName) {
 		this.leadName = leadName;
 	}
 
+	/**
+	 * Accessor for duration
+	 * @return
+	 */
 	public double getDuration() {
 		return duration;
 	}
 
+	/**
+	 * Mutator for duration
+	 * @param duration
+	 */
 	public void setDuration(double duration) {
 		this.duration = duration;
 	}
