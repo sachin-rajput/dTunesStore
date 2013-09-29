994c31bec4fb0b2fdafd0baba22476f3906c5d65
sachin-rajput <sachin.rajput@hotmail.com>
2013-09-29 16:21:05 -0700

    Created a new Interface

---
+++ src/dTunesStore/util/StoreOperationsInterface.java
@@ -0,0 +1,13 @@
+package dTunesStore.util;
+
+import dTunesStore.dataStore.MusicInfo;
+
+public interface StoreOperationsInterface {
+	
+	public void displayData();
+	
+	public void streamOutput(String type,Object currentObj);
+	
+	
+
+}
