385e148b991b51c86994d9c9eb6d1843425568b5
amol-mankar <amankar1@binghamton.edu>
2013-09-28 23:30:11 -0400

    Added script to generate DataFile and SearchFile

--- src/dTunesStore/util/Helper.java
+++ src/dTunesStore/util/Helper.java
@@ -1,6 +1,7 @@
 package dTunesStore.util;
 
 import java.io.*;
+import java.util.Random;
 
 import dTunesStore.dataStore.MusicInfo;
 
@@ -53,4 +54,50 @@ public class Helper {
 	return cnt;
 	}
 	
-}
+	public void createEntriesScript(String fileName,int noOfEntries){
+		try {
+			 Random generator = new Random();
+			 File ff = new File(fileName);
+			 if(!ff.exists() || ff.length()==0){
+				 ff.createNewFile();
+				 BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
+				 for (int i = 1; i <= noOfEntries; i++) {
+	            	double r = Math.round((generator.nextDouble()*5 + 4) * 10) / 10.0;
+	                out.write("name" + i +" " + "album" + i +" " + "singer" + i + " " + r);
+	                out.newLine();
+	            }
+	            out.close();
+			 }
+	        } 
+		catch (IOException e) {
+	        	
+	        }
+	    }
+	
+	public void createSearchScript(String fileName,int noOfEntries,int outOfBoundEntries,int noOfSearchTerms){
+		try {
+			 Random number = new Random();
+			 Random picker  = new Random();
+			 File ff = new File(fileName);
+			 String[] searchTerms = {"name","album","singer"};
+			 if(!ff.exists() || ff.length()==0){
+				 ff.createNewFile();
+				 BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
+				 for (int i = 1; i <= noOfSearchTerms; i++) {
+					 //5 + (int)(Math.random() * ((10 - 5) + 1))
+					 int r = (int)(Math.random() * ((outOfBoundEntries - 1) +1));
+	            	//int r = Math.round((number.nextInt()*1 + outOfBoundEntries));
+	                out.write(searchTerms[picker.nextInt(searchTerms.length)]+r);
+	                out.newLine();
+	            }
+	            out.close();
+			 }
+	        } 
+		catch (IOException e) {
+	        	
+	        }
+	    }
+
+	}
+	
+
