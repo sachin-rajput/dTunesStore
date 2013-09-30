270bfc0d69b6524a58f0c94cf25e7f62857effcb
sachin-rajput <sachin.rajput@hotmail.com>
2013-09-29 17:25:46 -0700

    Added comments

--- src/dTunesStore/dataStore/SearchWorker.java
+++ src/dTunesStore/dataStore/SearchWorker.java
@@ -14,6 +14,13 @@ public class SearchWorker implements Runnable {
 	private MusicStore musicStore;
 	private Results results;
 	
+	/**
+	 * Constructor for SearchWorker 
+	 * @param fileName - searchFilename
+	 * @param searchThreads - number of threads for searchworker
+	 * @param results - object of the class where we will save the data structure 
+	 * @param musicStore - object from which we will read the data
+	 */
 	public SearchWorker(String fileName, int searchThreads, Results results,MusicStore musicStore) {
 		Debug.print_debug(4,"SearchWorker constructor called.");
 		this.musicStore = musicStore;
@@ -24,17 +31,9 @@ public class SearchWorker implements Runnable {
 		
 
 		for (int i = 0; i < searchThreads; i++) {
-
-			// new PopulateWorker(threads,fileName,from,to);
 			t = new Thread(this, "Thread Created!");
-			//System.out.println("Child thread: " + t + " with id: " + t.getId());
-			// System.out.println("Thread config: from" + this.from + " to: " +
-			// this.to);
-			// this.thread_no = t.getId();
-
 			t.start();
 			this.threads.add(t);
-
 		}
 
 		for (Thread th : this.threads) {
@@ -51,28 +50,19 @@ public class SearchWorker implements Runnable {
 		 */
 		this.reader.close_file();
 		
-		/***
-		 * Lets print the entire DataStructure which we saved
-		 */
-		//System.out.println("---------- Results Data Structure -----------------");
-		//this.results.displayData();
-
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		
-		/***
-		 * Read the file 
-		 */
 		int currentThreadId = (int) Thread.currentThread().getId();
 		Debug.print_debug(3,"Thread - " + currentThreadId +"'s run method called from SearchWorker.");
-		//System.out.println("Child thread: " + Thread.currentThread().getId());
 		
+		/**
+		 * Read the data from datastructure and search from it and save the found data 
+		 * to result datastructure
+		 */
 		this.reader.read_file(currentThreadId,"search");
-		//this.reader.search_file(currentThreadId);
-		
-		//System.out.println("Exiting Child thread.");
 	}
 }
