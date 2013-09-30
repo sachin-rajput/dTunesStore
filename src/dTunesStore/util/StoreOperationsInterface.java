f861fb1b18c7c576a7a23184c32f9b2c8a0d0b63
amol-mankar <amankar1@binghamton.edu>
2013-09-29 21:17:15 -0400

    Update StoreOperationsInterface.java

--- src/dTunesStore/util/StoreOperationsInterface.java
+++ src/dTunesStore/util/StoreOperationsInterface.java
@@ -1,13 +1,18 @@
 package dTunesStore.util;
 
-import dTunesStore.dataStore.MusicInfo;
 
 public interface StoreOperationsInterface {
-	
+	/**
+	 * This method can be implemented by any class as per the requirement of the data structure
+	 */
 	public void displayData();
 	
+	/**
+	 * This method takes an object type as a parameter, so any class implementing this interface
+	 * can cast the object as per the need
+	 * @param type
+	 * @param currentObj - object that should be displayed
+	 */
 	public void streamOutput(String type,Object currentObj);
 	
-	
-
 }
